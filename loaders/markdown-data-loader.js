9bdad1f92a5f80ec78e8c69a608a52161df10f43
Muqing <muqing@qunhemail.com>
2018-04-22 12:04:23 +0800

    add markdown file tree reader

--- loaders/markdown-data-loader.js
+++ loaders/markdown-data-loader.js
@@ -1,9 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+function isDirectory(filename) {
+    return fs.statSync(filename).isDirectory();
+}
+
+function getAllMarkdown(mdSource) {
+    if (!isDirectory(mdSource)) {
+        throw new Error('should be a directory path');
+    }
+    
+    return fs.readdirSync(mdSource).reduce((fileTree, filename) => {
+        const mdPath = path.resolve(mdSource, filename);
+        const basename = path.basename(mdPath);
+        if (isDirectory(mdPath)) {
+            const subFileTree = getAllMarkdown(mdPath);
+            if (Object.keys(subFileTree) === 0) {
+                return fileTree;
+            }
+            fileTree[basename] = subFileTree;
+        } else if (basename.match(/\.md$/)) {
+            fileTree[`${basename}`] = mdPath;
+        }
+
+        return fileTree;
+    }, {})
+}
+
 module.exports = function markdownDataLoader() {
     if (this.cacheable) {
         this.cacheable();
     }
 
+    const fileTree = getAllMarkdown(path.join(process.cwd(), './posts'));
+
     return `
-        module.exports = ${JSON.stringify({ a: 1 })}
+        module.exports = ${JSON.stringify(fileTree, null, 2)}
     `;
 }
